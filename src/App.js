4dde9ffbe2accbb36b192740c9035e92dbb04f51
santiagodevrel <santiago@chainsafe.io>
2024-09-28 14:48:39 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+//1. import web3
+import { Web3 } from "web3";
+
+//2. import swisstronik plugin
+import { SwisstronikPlugin } from "@swisstronik/web3-plugin-swisstronik";
+
+//3. import ABI of counter contract
+import ABI from "./abi.mjs";
 
 function App() {
+  //4. initialize the web3 object
+  const web3 = new Web3(window.ethereum);
+
+  //5. register swisstronik plugin
+  web3.registerPlugin(new SwisstronikPlugin("https://json-rpc.testnet.swisstronik.com"));
+
+  //6. initialize contract
+  const contract = new web3.eth.Contract(ABI, "0xef05a01bd64dda4962687cbae843c6cbf5b8cc14");
+
+  //7. call the contract
+  async function returnCount() {
+    console.log(await web3.eth.getChainId());
+
+    const result = await contract.methods.returnCount().call();
+    console.log("current count", result);
+  }
+
+  async function increaseCount() {
+    // connect user's account
+    const accounts = await web3.eth.requestAccounts();
+
+    // call the writing function "increaseCount"
+    const receipt = await contract.methods.increaseCount().send({ from: accounts[0] });
+    console.log("tx receipt:", receipt);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={returnCount}>Count (reading fn)</button>
+        <button onClick={increaseCount}>Increase Count (writing fn)</button>
       </header>
     </div>
   );
---
+++ src/abi.mjs
@@ -0,0 +1,24 @@
+const ABI = [
+  {
+    inputs: [],
+    name: "increaseCount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "returnCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export default ABI;
